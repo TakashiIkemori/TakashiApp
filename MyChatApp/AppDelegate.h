295c904567a9ed2759310524e9e21deda40f3998
TakashiIkemori <daiyamatohou_0416@yahoo.co.jp>
2015-09-11 10:57:55 +0900

    init

---
+++ MyChatApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyChatApp
+//
+//  Created by 池森　崇 on 2015/09/06.
+//  Copyright (c) 2015年 myname. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyChatApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyChatApp
+//
+//  Created by 池森　崇 on 2015/09/06.
+//  Copyright (c) 2015年 myname. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
